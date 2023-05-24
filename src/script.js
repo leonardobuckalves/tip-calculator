2f03bc7a6f456bd6ab4dd38dbdf34cb194c020f5
leonardobalves <leonardobuckalves@gmail.com>
2023-05-24 20:00:51 -0300

    Major changes at script.js and style.css
    -Added new layout
    -Changed script to meet new project specification

--- src/script.js
+++ src/script.js
@@ -1,72 +1,75 @@
 const root = document.querySelector('#root');
 const main = document.createElement('div');
 
-const userBalance = 0;
-const userMonthBalance = 0;
-const differentialPercentage = 0;
+main.setAttribute('id', 'container__main');
 
-main.setAttribute('id', 'container--main');
+const leftSection = document.createElement('div');
+const rightSection = document.createElement('div');
 
-const balanceSection = document.createElement('div');
-balanceSection.setAttribute('id', 'container--balance');
-balanceSection.classList.add('flex', 'center', 'spaced');
-balanceSection.innerHTML = `
-    <div class="spaced">
-        <div>
-            My balance
-        </div>
-        <div class="fontSize">
-            R$${userBalance}
-        </div>
-    </div>
-    <div class="flex spaced">
-        <div id="circle"></div>
-        <div id="circle-right"></div>
-    </div>
+leftSection.setAttribute('id', 'container__main_left');
+leftSection.classList.add('padded');
+
+rightSection.setAttribute('id', 'container__main_right');
+rightSection.classList.add('padded');
+
+const billDiv = document.createElement('div');
+const tipSelect = document.createElement('div');
+const peopleNum = document.createElement('div');
+
+billDiv.innerHTML = `
+    <div>Bill</div>
+    <div><input placeholder="$" /></div>
 `
-
-const chart = `
-    <div class="flex row">
-        <div class="spaced">mon</div>
-        <div class="spaced">tue</div>
-        <div class="spaced">wed</div>
-        <div class="spaced">thu</div>
-        <div class="spaced">fri</div>
-        <div class="spaced">sat</div>
-        <div class="spaced">sun</div>
-    </div>
-`
-
-const chartSection = document.createElement('div');
-chartSection.setAttribute('id', 'container--chart-section');
-chartSection.classList.add('flex', 'center', 'spaced');
-chartSection.innerHTML = `
+tipSelect.innerHTML = `
+    <div>Select Tip %</div>
     <div>
-        <div>
-            <div class="spaced"><h3>Spending - Last 7 days</h3></div>
-            <div class="spaced">${chart}</div>     
-        </div>
-        <div id="container--chart-section-bottom" class="spaced">
-            <div>
-                <div>
-                    Total this month
-                </div>
-                <div>
-                    R$${userMonthBalance}
-                </div>
-            </div>
-            <div>
-                <div>
-                    ${differentialPercentage}%
-                </div>
-                <div>
-                    From last Month
-                </div>
-            </div>
-        </div>
+        <button>5%</button>
+        <button>10%</button>
+        <button>15%</button>
+        <button>25%</button>
+        <button>50%<button>
+        <input placeholder="Custom" />
     </div>
 `
+peopleNum.innerHTML = `
+    <div>
+        Number of People
+    </div>
+    <div>
+        <input placeholder="0" />
+    </div>
+`
+
+const tipAmount = document.createElement('div');
+const totalPerPerson = document.createElement('div');
+
+tipAmount.innerHTML = `
+    <div>
+        Tip amount
+    </div>
+    <div>
+        $0.00
+    </div>
+`
+
+totalPerPerson.innerHTML = `
+    <div>
+        Total
+    </div>
+    <div>
+        $0.00
+    </div>
+`
+
 
 root.appendChild(main);
-main.appendChild(balanceSection);
-main.appendChild(chartSection);
+
+main.appendChild(leftSection);
+main.appendChild(rightSection);
+
+leftSection.appendChild(billDiv);
+leftSection.appendChild(tipSelect);
+leftSection.appendChild(peopleNum);
+
+rightSection.appendChild(tipAmount);
+rightSection.appendChild(totalPerPerson);

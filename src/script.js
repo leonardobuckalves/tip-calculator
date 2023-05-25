ab1cc8bf638e213accd3434a4c6838bd5bee9dd3
leonardobalves <leonardobuckalves@gmail.com>
2023-05-24 22:31:03 -0300

    added eventListener for inputs and buttons

--- src/script.js
+++ src/script.js
@@ -16,19 +16,23 @@ const billDiv = document.createElement('div');
 const tipSelectDiv = document.createElement('div');
 const peopleNumDiv = document.createElement('div');
 
+let bill;
+let tipAmount;
+let numOfPeople;
+
 billDiv.innerHTML = `
-    <div>Bill</div>
-    <div><input placeholder="$" /></div>
+    <div id="billable">Bill ${bill}</div>
+    <div><input type="text" placeholder="$" id="billInput" /></div>
 `
 tipSelectDiv.innerHTML = `
     <div>Select Tip %</div>
     <div>
-        <button>5%</button>
-        <button>10%</button>
-        <button>15%</button>
-        <button>25%</button>
-        <button>50%<button>
-        <input placeholder="Custom" />
+        <button id="btn-5" value="5">5%</button>
+        <button id="btn-10" value="10">10%</button>
+        <button id="btn-15" value="15">15%</button>
+        <button id="btn-25" value="25">25%</button>
+        <button id="btn-50" value="50">50%<button>
+        <input placeholder="Custom" id="tipAmountInput" />
     </div>
 `
 peopleNumDiv.innerHTML = `
@@ -36,7 +40,7 @@ peopleNumDiv.innerHTML = `
         Number of People
     </div>
     <div>
-        <input placeholder="0" />
+        <input placeholder="0" id="numPeopleInput"/>
     </div>
 `
 
@@ -44,12 +48,15 @@ const tipAmountDiv = document.createElement('div');
 const totalPerPersonDiv = document.createElement('div');
 const resetButtonDiv = document.createElement('div');
 
+let tipAmountPerPeople = 0;
+let totalPerPerson = 0;
+
 tipAmountDiv.innerHTML = `
     <div>
         Tip amount
     </div>
     <div>
-        $0.00
+        $${tipAmountPerPeople}
     </div>
 `
 
@@ -58,7 +65,7 @@ totalPerPersonDiv.innerHTML = `
         Total
     </div>
     <div>
-        $0.00
+        $${totalPerPerson}
     </div>
 `
 
@@ -80,3 +87,55 @@ leftSection.appendChild(peopleNumDiv);
 rightSection.appendChild(tipAmountDiv);
 rightSection.appendChild(totalPerPersonDiv);
 rightSection.appendChild(resetButtonDiv);
+
+const billInput = document.querySelector('#billInput'); 
+const billable = document.querySelector('#billable'); 
+
+billInput.addEventListener("input", () => {
+    bill = billInput.value;
+    console.log(bill);
+    billable.innerHTML = bill;
+});
+
+const btn5 = document.querySelector('#btn-5');
+const btn10 = document.querySelector('#btn-10');
+const btn15 = document.querySelector('#btn-15');
+const btn25 = document.querySelector('#btn-25');
+const btn50 = document.querySelector('#btn-50');
+const tipAmountInput = document.querySelector('#tipAmountInput');
+
+btn5.addEventListener("click", () => {
+    tipAmount = btn5.value;
+    console.log(tipAmount);
+});
+
+btn10.addEventListener("click", () => {
+    tipAmount = btn10.value;
+    console.log(tipAmount);
+});
+
+btn15.addEventListener("click", () => {
+    tipAmount = btn15.value;
+    console.log(tipAmount);
+});
+
+btn25.addEventListener("click", () => {
+    tipAmount = btn25.value;
+    console.log(tipAmount);
+});
+
+btn50.addEventListener("click", () => {
+    tipAmount = btn50.value;
+    console.log(tipAmount);
+});
+
+tipAmountInput.addEventListener("input", () => {
+    tipAmount = tipAmountInput.value;
+    console.log(tipAmount);
+});
+
+const numPeopleInput = document.querySelector('#numPeopleInput');
+numPeopleInput.addEventListener("input", () => {
+    numOfPeople = numPeopleInput.value;
+    console.log(numOfPeople);
+});
